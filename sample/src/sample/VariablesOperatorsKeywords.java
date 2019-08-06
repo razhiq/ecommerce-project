b1d2fb7a504508bcf075b4fc5ede79ffde74e00f
Razhiq <Razhiq@192.168.1.21>
2019-08-06 14:38:36 +0530

    Razhiq don

---
+++ sample/src/sample/VariablesOperatorsKeywords.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class VariablesOperatorsKeywords {
+
+	public static void main(String[] args) {
+		
+		String name = "Dinesh";
+		char c = 'd';
+		String r= "dd";
+		int age = 23;
+		boolean gs= false;
+		double ff= 23.0;
+		float s= 23.0f;
+		
+		//Arithmetic operators
+		int a=10;
+		int b=6;
+		int ch=a+b;
+		System.out.println(ch);
+		
+				
+	}
+}
